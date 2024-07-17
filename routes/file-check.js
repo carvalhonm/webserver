23e842d0c2fe1016d120a0f921a9eb2d413338f9
Nuno Carvalho <nunocarvalho@w4msolutions.com>
2024-07-17 22:39:34 +0100

    feat: #4 - check gpt

--- routes/file-check.js
+++ routes/file-check.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const exec = require('child_process');
 const uri = require('uri-js');
 const log = require('../services/log');
 const Redis = require('../services/redis');
